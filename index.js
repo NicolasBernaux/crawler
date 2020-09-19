abc33814dfdc18d5bdea0da6d88ee592e3800d55
NicolasBernaux <bernaux.n@gmail.com>
2020-09-20 00:14:25 +0200

    Add server

---
+++ index.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser')
+const consola = require('consola')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+async function start() {
+  // Give usefull middleware to express
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(morgan('combined'))
+
+  app.use('/', async (req, res) => {
+    res.send('Yes')
+  })
+
+  // Listen the server
+  app.set('port', process.env.PORT || 3000)
+  const server = app.listen(app.get('port'))
+
+  consola.ready({
+    message: `🚀 Server listening on http://localhost:${server.address().port}`,
+    badge: true,
+  })
+}
+start()
