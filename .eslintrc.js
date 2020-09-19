bd112cdbbed7ccb634bb7a9317fb351a7950c3ac
NicolasBernaux <bernaux.n@gmail.com>
2020-09-19 23:14:33 +0200

    Add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
